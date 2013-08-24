f0fb7fc49ea41a2e532b025f5ba63321e15fba5f
Burt Holzman <burt@fnal.gov>
2013-08-23 20:03:13 -0500

    Manually link one binary that the algorithm fails for

--- frontend-git-link.sh
+++ frontend-git-link.sh
@@ -8,11 +8,15 @@ git checkout v3_2_rc1
 
 sbinfiles="glidecondor_addDN glidecondor_createSecCol glidecondor_createSecSched \
            checkFrontend glidecondor_addDN glidecondor_createSecCol \
-           glidecondor_createSecSched glideinFrontend glideinFrontendElement.py \
+           glidecondor_createSecSched glideinFrontendElement.py \
            reconfig_frontend stopFrontend"
 
 binfiles="glidein_cat glidein_gdb glidein_interactive glidein_ls glidein_ps glidein_status \
           glidein_top wmsTxtView wmsXMLView"
+
+rm -f /usr/sbin/glideinFrontend
+real_loc=`find /usr/lib/python2.6/site-packages/glideinwms -type f -name glideinFrontend.py`
+ln -s ${real_loc} /usr/sbin/glideinFrontend
 
 for x in $sbinfiles
   do
